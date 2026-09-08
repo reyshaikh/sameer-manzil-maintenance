28f3090be04c015b8fe9ed3bb634e0ff32cb27b4
reyshaikh <70897345+reyshaikh@users.noreply.github.com>
2026-09-08 13:11:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -157,7 +157,33 @@ def pending():
         g['months'].append(d['Month']); g['total']+=float(d['Amount'])
     return render_template('pending.html', grouped=grouped, money=money)
 
+@app.route("/google-test")
+def google_test():
+    import json
+    import os
+    import gspread
+    from google.oauth2.service_account import Credentials
 
+    scope = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
+
+    creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+
+    creds = Credentials.from_service_account_info(
+        creds_dict,
+        scopes=scope
+    )
+
+    client = gspread.authorize(creds)
+
+    sheet = client.open(
+        os.environ["GOOGLE_SHEET_NAME"]
+    )
+
+    return f"✅ Connected Successfully: {sheet.title}"
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
